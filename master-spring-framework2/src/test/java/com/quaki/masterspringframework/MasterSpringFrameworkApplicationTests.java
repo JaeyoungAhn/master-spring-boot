6bd5b33531d2e2ef0fbec7c22135a9dbfb084000
JaeyoungAhn <quaki@naver.com>
2023-04-02 03:31:16 +0900

    update

---
+++ master-spring-framework2/src/test/java/com/quaki/masterspringframework/MasterSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quaki.masterspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
