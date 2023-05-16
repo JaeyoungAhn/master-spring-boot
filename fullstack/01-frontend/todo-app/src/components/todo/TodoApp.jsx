1287b6e7f418fd678e510c935d4f5dc15115b30d
JaeyoungAhn <quaki@naver.com>
2023-05-16 10:31:15 +0900

    added bootstrap to style todos

--- fullstack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ fullstack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -7,8 +7,8 @@ export default function TodoApp() {
     return (
         <>
             <div className="TodoApp">
-                <HeaderComponent></HeaderComponent>
                 <BrowserRouter>
+                    <HeaderComponent></HeaderComponent>
                     <Routes>
                         <Route path='/' element={<LoginComponent></LoginComponent>}/>
                         <Route path='/login' element={<LoginComponent></LoginComponent>}/>
@@ -19,8 +19,8 @@ export default function TodoApp() {
                         <Route path='*' element={<ErrorComponent/>}/>
                         
                     </Routes>
+                    <FooterComponent></FooterComponent>
                 </BrowserRouter>
-                <FooterComponent></FooterComponent>
             </div>
         </>
     )
@@ -142,10 +142,10 @@ function ListTodosComponent() {
 
     return (
         <>
-            <div className="ListTodosComponent">
+            <div className="container">
                 <h1>Things You Want To Do!</h1>
                 <div>
-                    <table>
+                    <table className="table">
                         <thead>
                             <tr>
                                 <td>ID</td>
@@ -178,17 +178,50 @@ function ListTodosComponent() {
 
 function HeaderComponent() {
     return (
-            <div className="header">
-                Header <hr />
-            </div>
+            // <header className="header">
+            //     <div className="container">
+            //         <ul className="navbar-nav">
+            //             <li className="nav-item"><a className="nav-link" href="https://www.spring.com">spring</a></li>
+
+            //             <li className="nav-item"><Link className="nav-link" to="/welcome/springuser">Home</Link></li>
+            //             <li className="nav-item"><Link className="nav-link" to="/todos">Todos</Link></li>
+
+            //             <li className="nav-item"><Link className="nav-link" to="/logout">logout</Link></li>
+            //             <li className="nav-item"><Link className="nav-link" to="/">login</Link></li>
+                        
+            //         </ul>
+            //     </div>
+            // </header>
+
+            <header className="border-bottom border-light border-5 mb-5 p-2">
+                <div className="container">
+                    <div className="row">
+                        <nav className="navbar navbar-expand-lg">
+                            <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.in28minutes.com">in28minutes</a>
+                            <div className="collapse navbar-collapse">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
+                                    <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                                </ul>
+                            </div>
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
     )
 }
 
 function FooterComponent() {
     return (
-            <div className="footer">
-                <hr /> Footer 
-            </div>
+            <footer className="footer">
+                <div class="container">
+                    Your Footer
+                </div>
+            </footer>
     )
 }
 
